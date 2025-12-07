7bf0bd5128dd99d5306f42c4fd96b3d5df2a92c5
Egor-Chirikov <echirikov2005@gmail.com>
2025-12-07 14:36:38 +0300

    end

--- main.py
+++ main.py
@@ -17,7 +17,43 @@ DS_TEST_HORSE_DIR = '/horse_512/data/validation'
 WORK_SIZE = (200, 200)  # Размер изображений для нейронки
 NUM_CLASSES = 2 # Количество классов распознаваемых объектов
 
-def make_model():
+def load_data(ds_dir):
+    x_train = []
+    y_train = []
+    
+    tmp_dir = str(ds_dir + DS_TRAIN_BRAIN_DIR)
+    x_train, y_train = load_img(x_train, y_train, tmp_dir, 1)
+    
+    tmp_dir = str(ds_dir + DS_TRAIN_HORSE_DIR)
+    x_train, y_train = load_img(x_train, y_train, tmp_dir, 0)
+
+    x_test = []
+    y_test = []
+    
+    tmp_dir = str(ds_dir + DS_TEST_BRAIN_DIR)
+    x_test, y_test = load_img(x_test, y_test, tmp_dir, 1)
+    
+    tmp_dir = str(ds_dir + DS_TEST_HORSE_DIR)
+    x_test, y_test = load_img(x_test, y_test, tmp_dir, 0)
+    return x_train, y_train, x_test, y_test
+
+
+def load_img(x, y, dir, flg):
+    tmp_dir = str(dir)
+    filelist = os.listdir(tmp_dir)
+    for i in filelist:     
+        fname = str(tmp_dir+ "/"+  i)
+        img = cv2.imread(fname)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+        img = cv2.resize(img, WORK_SIZE)
+        img = np.expand_dims(img, axis=2) 
+        
+        x.append(img)
+        y.append(int(flg))  
+    return x, y
+
+if __name__ == '__main__':
     input_shape = (WORK_SIZE[0],WORK_SIZE[1],1) # Размерность входных данных. В данном случае изображения 200х200 пикселей 1 цвета (ЧБ).
     model = Sequential() # Создаём последовательную модель
    
@@ -42,44 +78,9 @@ def make_model():
     model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(),
                   metrics=['accuracy']) # Компилируем модель
     print(model.summary()) # Печатаем в консоль структуру нашей модели с размерами данных между слоями и числом параметров
-    return model
 
-def load_data(ds_dir):
-    x_train = []
-    y_train = []
     
-    tmp_dir = str(ds_dir + DS_TRAIN_BRAIN_DIR)
-    x_train, y_train = load_img(x_train, y_train, tmp_dir, 1)
     
-    tmp_dir = str(ds_dir + DS_TRAIN_HORSE_DIR)
-    x_train, y_train = load_img(x_train, y_train, tmp_dir, 0)
-
-    x_test = []
-    y_test = []
-    
-    tmp_dir = str(ds_dir + DS_TEST_BRAIN_DIR)
-    x_test, y_test = load_img(x_test, y_test, tmp_dir, 1)
-    
-    tmp_dir = str(ds_dir + DS_TEST_HORSE_DIR)
-    x_test, y_test = load_img(x_test, y_test, tmp_dir, 0)
-    return x_train, y_train, x_test, y_test
-
-def load_img(x, y, dir, flg):
-    tmp_dir = str(dir)
-    filelist = os.listdir(tmp_dir)
-    for i in filelist:     
-        fname = str(tmp_dir+ "/"+  i)
-        img = cv2.imread(fname)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-        img = cv2.resize(img, WORK_SIZE)
-        img = np.expand_dims(img, axis=2) 
-        
-        x.append(img)
-        y.append(int(flg))  
-    return x, y
-
-def learn_mdl(model):
     ds_dir = "dataset"
     x_train, y_train, x_test, y_test = load_data(ds_dir)
 
@@ -100,8 +101,3 @@ def learn_mdl(model):
     print("Baseline Error: %.2f%%" % (100 - score[1] * 100))
     model.save('testIT3.h5') # Сохраняем в файл
     print("Модель сохранена как testIT3.h5")
-
-
-if __name__ == '__main__':
-    model = make_model() 
-    learn_mdl(model) 
